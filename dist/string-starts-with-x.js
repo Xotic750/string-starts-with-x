b9ecd70f3bbed0cd137f380f665773c76308302b
Graham Fairweather <xotic750@gmail.com>
2019-07-22 01:25:46 +0200

    :bookmark: v2.0.4
    Auto build

--- dist/string-starts-with-x.js
+++ dist/string-starts-with-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-19T22:37:07.836Z",
+  "date": "2019-07-21T23:25:40.393Z",
   "describe": "",
   "description": "Determines whether a string begins with the characters of a specified string.",
   "file": "string-starts-with-x.js",
   "hash": "3781fc8b2e4193e3baa7",
   "license": "MIT",
-  "version": "2.0.3"
+  "version": "2.0.4"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
